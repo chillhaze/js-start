7807cc10f990206300793ef1c263f6cc0a29a8c3
chillhaze <65725016+chillhaze@users.noreply.github.com>
2021-04-22 19:43:47 +0300

    task-1

--- js/script.js
+++ js/script.js
@@ -1,1 +1,20 @@
+// let array = ['Mango', 'Poly', 'Ajax'];
 
+// function getExtremeElements(array) {
+//   let newArray = [array[0],[array.length-1]];
+//   console.log(newArray);
+
+// }
+// console.log(array[0]);
+// console.log(array.length);
+
+// console.table(array);
+
+let title = 'Десять секретов JavaScript';
+function slugify(title) {
+  // Пиши код ниже этой строки
+  const normalzeTitle = title.toLowerCase();
+  console.log(normalzeTitle);
+ return title;
+  // Пиши код выше этой строки
+}
