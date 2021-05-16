7933032448fe43d47399346c575f3e4c4387d552
chillhaze <65725016+chillhaze@users.noreply.github.com>
2021-05-16 21:46:12 +0300

    js/theory-4 done

--- js/script.js
+++ js/script.js
@@ -1,1 +1,141 @@
+// ------------------------------------- Модуль 4-3
+// const pizzaPalace = {
+//   pizzas: ["Ультрасыр", "Аль Копчино", "Четыре нарезона"],
+//   order(pizzaName, onSuccess, onError) {
+//     console.log(pizzaName);
 
+//     if (!this.pizzas.includes(pizzaName)) {
+//       return onError(`В ассортименте нет пиццы с названием ${pizzaName}.`);
+//     }
+//     return onSuccess(pizzaName);
+//   },
+// };
+// // Пиши код выше этой строки
+
+// // Колбэк для onSuccess
+// function makePizza(pizzaName) {
+//   return `Ваш заказ принят. Готовим пиццу ${pizzaName}.`;
+// }
+
+// // Колбэк для onError
+// function onOrderError(error) {
+//   return `Ошибка! ${error}`;
+// }
+
+// // Вызовы метода с колбэками
+// pizzaPalace.order("Аль Копчино", makePizza, onOrderError);
+// pizzaPalace.order("Четыре нарезона", makePizza, onOrderError);
+// pizzaPalace.order("Биг майк", makePizza, onOrderError);
+// pizzaPalace.order("Венская", makePizza, onOrderError);
+
+// console.log(pizzaPalace.order("Аль Копчино", makePizza, onOrderError));
+// console.log(pizzaPalace.order("Четыре нарезона", makePizza, onOrderError));
+// console.log(pizzaPalace.order("Биг майк", makePizza, onOrderError));
+// console.log(pizzaPalace.order("Венская", makePizza, onOrderError));
+
+// ------------------------------------- Модуль 4-7 матод call
+
+// const orders = [
+//   { email: "solomon@topmail.ua", dish: "Burger" },
+//   { email: "artemis@coldmail.net", dish: "Pizza" },
+//   { email: "jacob@mail.com", dish: "Taco" },
+// ];
+
+// // Пиши код ниже этой строки
+// function composeMessage(position) {
+//   return `Готовим ${this.dish} для ${this.email}. Ваш заказ ${position}-й в очереди.`;
+// }
+
+// const messages = [];
+
+// for (let i = 0; i < orders.length; i++) {
+//   messages.push(composeMessage.call(orders[i], i + 1));
+//   console.log(orders[i]);
+// }
+
+// console.log(messages);
+
+// ------------------------------------- Модуль 4-8 матод apply
+
+// const orders = [
+//   { email: "solomon@topmail.ua", dish: "Burger" },
+//   { email: "artemis@coldmail.net", dish: "Pizza" },
+//   { email: "jacob@mail.com", dish: "Taco" },
+// ];
+
+// // Пиши код ниже этой строки
+// function composeMessage(position) {
+//   return `Готовим ${this.dish} для ${this.email}. Ваш заказ ${position}-й в очереди.`;
+// }
+
+// const messages = [];
+
+// for (let i = 0; i < orders.length; i++) {
+//   messages.push(composeMessage.apply(orders[i], [i + 1]));
+//   console.log(orders[i]);
+// }
+
+// console.log(messages);
+
+// ------------------------------------- Модуль 4-9
+
+// const pizzaPalace = {
+//   company: "Pizza Palace",
+// };
+
+// // console.log(pizzaPalace);
+
+// const burgerShack = {
+//   company: "Burger Shack",
+// };
+
+// function composeMessage(customerName) {
+//   return `${customerName}, всегда рады вас видеть в «${this.company}».`;
+// }
+// // Пиши код ниже этой строки
+
+// const pizzaPalaceComposer = composeMessage.bind(pizzaPalace);
+// const pizzaPalaceMessage = console.log(pizzaPalaceComposer("Манго"));
+
+// const burgerShackComposer = composeMessage.bind(burgerShack);
+// const burgerShackMessage = console.log(burgerShackComposer("Поли"));
+
+// ------------------------------------- Модуль 4-10
+
+const service = {
+  mailingList: ["mango@mail.com", "poly@hotmail.de", "ajax@jmail.net"],
+  subscribe(email) {
+    this.mailingList.push(email);
+    return `Почта ${email} добавлена в рассылку.`;
+  },
+  unsubscribe(email) {
+    this.mailingList = this.mailingList.filter((e) => e !== email);
+    return `Почта ${email} удалена из рассылки.`;
+  },
+};
+
+function logAndInvokeAction(email, action) {
+  console.log(`Выполняем действие с ${email}.`);
+  return action(email);
+}
+
+const firstInvoke = logAndInvokeAction(
+  "kiwi@mail.uk",
+  service.subscribe.bind(service)
+);
+console.log(firstInvoke);
+// Почта kiwi@mail.uk добавлена в рассылку.
+
+console.log(service.mailingList);
+/* ['mango@mail.com', 
+    'poly@hotmail.de', 
+    'ajax@jmail.net', 
+    'kiwi@mail.uk']*/
+const secondInvoke = logAndInvokeAction(
+  "poly@hotmail.de",
+  service.unsubscribe.bind(service)
+);
+console.log(secondInvoke);
+// Почта poly@hotmail.de удалена из рассылки.
+
+console.log(service.mailingList); // ['mango@mail.com', 'ajax@jmail.net', 'kiwi@mail.uk']
